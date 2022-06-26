abf1524ffece81aa9d4a47f677edf985d1c0f2fa
José Manuel <Questionsleep123@gmail.com>
2022-06-25 23:13:25 -0500

    Agregado fondo y cambiada la resolución

--- 1_Frogge/11_sounds.py
+++ 1_Frogge/11_sounds.py
@@ -13,17 +13,22 @@ pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.mixer.set_num_channels(64)
 pygame.display.set_caption('Pygame Window')  # Le da un nombre a la pantalla
 
-WINDOW_SIZE = (600, 400)  # Establece el tamaño de la pantalla
+WINDOW_SIZE = (800, 640)  # Establece el tamaño de la pantalla
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # Inicializa la pantalla
 
-display = pygame.Surface((300, 200))
+display = pygame.Surface((400, 320))
 # Se puede proyectar una imagen en una superficie y escalar
 # esa superficie independientemente del resto de elemtnos
 
 
 #Objetos de fondo para el efect Parallax
-background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,70,50,400]],[0.5,[130,50,80,300]]]
+background_objects = [[0.25,[120,10,70,400]],
+                        [0.25,[50,120,550,400]],
+                        [0.5,[30,70,50,400]],
+                        [0.5,[130,50,80,300]],
+                        [0.75,[130,50,80,150]],
+                        [0.75,[800,50,80,400]]]
 
 # ColorKey es el color clave para definir la transparencia del sprite
 #player_image.set_colorkey((255,255,255))
@@ -31,6 +36,8 @@ background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,70,
 grass_image = pygame.image.load('data/images/grass.png')
 TILE_SIZE = grass_image.get_width()
 dirt_image = pygame.image.load('data/images/dirt.png')
+
+forest_bg = pygame.image.load('data/images/forest.png')
 
 flag_image1 = pygame.image.load( 'data/images/flag1.png' )
 flag_image2 = pygame.image.load( 'data/images/flag2.png' )
@@ -150,24 +157,30 @@ GAME_ON = True
 while GAME_ON:  # Loop del Juego
     display.fill((255, 255, 255))
     
+    display.blit( forest_bg, (0,0) )
+
     # play the grass sound only when
     if grass_sound_timer > 0:
         grass_sound_timer -= 1
 
     true_scroll[0] += (player_rect.x-true_scroll[0]-140)/20
-    true_scroll[1] += (player_rect.y-true_scroll[1]-110)/20
+    true_scroll[1] += (player_rect.y-true_scroll[1]-200)/20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
     
-    pygame.draw.rect(display,(0,0,0), pygame.Rect(0,120,300,80))
+    #pygame.draw.rect(display,(0,0,0), pygame.Rect(0,120,300,80))
     # Efecto Parallax
     for background_object in background_objects:
-        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0], 50, 400)
+        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],
+                                background_object[1][1]-scroll[1]*background_object[0], 
+                                50, 400)
+        if background_object[0] == 0.25:
+            pygame.draw.rect(display,(255, 0, 0), obj_rect)
         if background_object[0] == 0.5:
-            pygame.draw.rect(display,(255,80,255),obj_rect)
-        else:
-            pygame.draw.rect(display,(80,0,80),obj_rect)
+            pygame.draw.rect(display,(0, 255, 0), obj_rect)
+        if background_object[0] == 0.75:
+            pygame.draw.rect(display,(0, 0, 255 ), obj_rect)
 
     # Bucle para generar los tiles en la pantalla
     tile_rects = []
@@ -191,7 +204,7 @@ while GAME_ON:  # Loop del Juego
         y += 1
 
     #print( player_rect.x )
-    print(flag_pos)
+    #print(flag_pos)
 
     player_movement = [0, 0]
     if moving_right:
@@ -253,7 +266,7 @@ while GAME_ON:  # Loop del Juego
             damage_sound.play()
         
 
-    if  player_rect.y >= 250:
+    if  player_rect.y >= 200:
         player_rect.x = position_fall[0]
         player_rect.y = position_fall[1]-30
         air_timer = 0
@@ -322,7 +335,15 @@ while GAME_ON:  # Loop del Juego
 
         if death_count >= 3:
             print( "Perdiste" )
-            while( True ):
+            lose = True
+            while( lose ):
+                for event in pygame.event.get():  # Evento Loop
+                        if event.type == QUIT:  # Revisa si se cerró la pantalla
+                            pygame.quit()  # Detiene pygame
+                            sys.exit()  # Detiene el script
+                        if event.type == KEYDOWN:
+                            if event.key == K_ESCAPE:
+                                lose = False
                 display.blit( perdiste_image, (0,0) )
                 pygame.display.update()
                 surf = pygame.transform.scale(display,WINDOW_SIZE)
@@ -333,7 +354,15 @@ while GAME_ON:  # Loop del Juego
 
         if player_rect.x >= flag_pos - 15 and player_rect.x <= flag_pos + 15:
             print( "Ganaste" )
-            while( True ):
+            win = True
+            while( win ):
+                for event in pygame.event.get():  # Evento Loop
+                        if event.type == QUIT:  # Revisa si se cerró la pantalla
+                            pygame.quit()  # Detiene pygame
+                            sys.exit()  # Detiene el script
+                        if event.type == KEYDOWN:
+                            if event.key == K_k:
+                                win = False 
                 display.blit( ganaste_image, (0,0) )
                 pygame.display.update()
                 surf = pygame.transform.scale(display,WINDOW_SIZE)
@@ -343,8 +372,8 @@ while GAME_ON:  # Loop del Juego
             sys.exit()
 
 
-        print( "Ubicación x: " + str( player_rect.x ) )
-        print( "Ubicación y: " + str( player_rect.y ) )
+        #print( "Ubicación x: " + str( player_rect.x ) )
+        #print( "Ubicación y: " + str( player_rect.y ) )
     
     surf = pygame.transform.scale(display,WINDOW_SIZE)
     screen.blit(surf,(0,0))
